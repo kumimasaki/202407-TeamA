96c16b940b41fc8469b71c733156eb5460658a68
Yoyo977 <zengrongrong325@gmail.com>
2024-09-13 17:44:56 +0900

    1

--- TeamA/src/main/java/teama/com/controllers/LessonListController.java
+++ TeamA/src/main/java/teama/com/controllers/LessonListController.java
@@ -1,5 +1,41 @@
 package teama.com.controllers;
 
-public class LessonListController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import teama.com.models.entity.Admin;
+import teama.com.models.entity.Lesson;
+import teama.com.services.LessonService;
+
+@Controller
+public class LessonListController {
+	@Autowired
+	private HttpSession session;
+
+	@Autowired
+	private LessonService lessonService;
+
+	// 講座一覧画面を表示する
+	@GetMapping("/lesson/list")
+	public String getLessonList(Model model) {
+		// セッションからログインしている人の情報を取得
+		Admin admin = (Admin) session.getAttribute("loginAdminInfo");
+		// もし、admin==null ログイン画面にリダイレクトする
+		// そうでない場合
+		// ログインしている人の名前の情報を画面に渡して講座一覧のhtmlを表示。
+		if(admin == null) {
+			return "redirect:/admin/login";
+		}else{
+			//講座の情報を取得する。
+			List<Lesson> lessonList = lessonService.selectAllLessonList(admin.getAdminId());
+			model.addAttribute("adminName", admin.getAdminName());
+			model.addAttribute("lessonList",lessonList);
+			return "lesson_list.html";
+		}
+	}
 }
