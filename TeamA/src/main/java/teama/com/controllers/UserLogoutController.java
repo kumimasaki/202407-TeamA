7c8ce78aa8d4731dcfd6e5303c5bfaaaed44cef7
zcj5539 <1026611363@qq.com>
2024-09-25 16:07:37 +0900

    ユーザー登録の単体テスト

---
+++ TeamA/src/main/java/teama/com/controllers/UserLogoutController.java
@@ -0,0 +1,21 @@
+package teama.com.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class UserLogoutController {
+	@Autowired
+	private HttpSession session;
+	
+	//logout処理
+	@GetMapping("/user/logout")
+	public String userLogout() {
+		//セッションの無効化
+		session.invalidate();
+		return "redirect:/user/login";
+	}
+}
