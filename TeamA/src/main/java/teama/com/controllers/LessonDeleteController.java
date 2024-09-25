82334899e09fea298346bd224239f3cfb3fc1b9b
Yoyo977 <zengrongrong325@gmail.com>
2024-09-25 17:19:50 +0900

    925

--- TeamA/src/main/java/teama/com/controllers/LessonDeleteController.java
+++ TeamA/src/main/java/teama/com/controllers/LessonDeleteController.java
@@ -1,39 +1,48 @@
 package teama.com.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpSession;
 import teama.com.models.entity.Admin;
 import teama.com.services.LessonService;
 
+//@Controllerアノテーションの主な役割は、主にHTMLページなどのビューを生成すること(司令塔)
 @Controller
 public class LessonDeleteController {
+	// @Autowiredアノテーションをつけて、自動的にインターフェースを実装して、インスタンス化させて、ControllerでHttpSessionを使えるようにします
 	@Autowired
 	private LessonService lessonService;
 
 	@Autowired
+	// sessionは、ユーザー情報を、一時的にサーバー側で保持される、 どのページも共通もデータを使いたい時にもちいる機能、（名前、値）という形式で保管される
 	private HttpSession session;
 
+	// 削除画面の表示
 	@GetMapping("/lesson/delete/{lessonId}")
-	public String deleteByLesson(@PathVariable Long lessonId,Model model) {
+	// @PathVariable ブログIDを使ってデータベースからブログ情報を取得
+	public String deleteByLesson(@PathVariable Long lessonId, Model model) {
 		// セッションからログインしている人の情報をadminという変数に格納
 		Admin admin = (Admin) session.getAttribute("loginAdminInfo");
 		// もしadmin==null ログイン画面にリダイレクトする
 		if (admin == null) {
 			return "redirect:/admin/login";
 		} else {
-			// もしdeleteByLessonの結果がtrueの時に、lesson_delete_complete.htmlを遷移する
-			// そうでない場合、 編集画面にリダイレクトする
+			// もしdeleteByLessonの結果がtrueの場合
 			if (lessonService.deleteByLesson(lessonId)) {
+				// ユーザー名前を表示
 				model.addAttribute("adminName", admin.getAdminName());
+				// 削除完了画面を遷移する
 				return "lesson_delete_complete.html";
+				// そうでない場合
 			} else {
+				// adminnameを表示
 				model.addAttribute("adminName", admin.getAdminName());
+				// 編集画面にリダイレクトする
 				return "redirect:/lesson/edit/" + lessonId;
 			}
 		}
--- TeamA/src/main/java/teama/com/controllers/LessonEditController.java
+++ TeamA/src/main/java/teama/com/controllers/LessonEditController.java
@@ -1,12 +1,7 @@
 package teama.com.controllers;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Date;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -17,22 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import teama.com.models.entity.Admin;
 import teama.com.models.entity.Lesson;
 import teama.com.services.LessonService;
 
+//@Controllerアノテーションの主な役割は、主にHTMLページなどのビューを生成すること(司令塔)
 @Controller
 public class LessonEditController {
+	// @Autowiredアノテーションをつけて、自動的にインターフェースを実装して、インスタンス化させて、ControllerでHttpSessionを使えるようにします
 	@Autowired
 	private LessonService lessonService;
 
 	@Autowired
+	// sessionは、ユーザー情報を、一時的にサーバー側で保持される、 どのページも共通もデータを使いたい時にもちいる機能、（名前、値）という形式で保管される
 	private HttpSession session;
 
 	// 編集画面の表示
 	@GetMapping("/lesson/edit/{lessonId}")
+	// @PathVariable ブログIDを使ってデータベースからブログ情報を取得
 	public String getLessonEditPage(@PathVariable Long lessonId, Model model) {
 		// セッションからログインしている人の情報をadminという変数に格納
 		Admin admin = (Admin) session.getAttribute("loginAdminInfo");
@@ -43,14 +41,18 @@ public class LessonEditController {
 			// 編集画面に表示させる情報を変数に格納 lesson
 			Lesson lesson = lessonService.lessonEditCheck(lessonId);
 			// もしlesson == nullだったら、商品一覧ページにリダイレクトする
-			// そうでない場合、編集画面に編集する内容を渡す
-			// 編集画面を表示
+
 			if (lesson == null) {
+				// adminnameを表示
 				model.addAttribute("lesson", lesson);
+				// 編集画面に編集する内容を渡す
 				return "redirect:/lesson/list";
+				// そうでない場合
 			} else {
+				// adminnameを表示
 				model.addAttribute("adminName", admin.getAdminName());
 				model.addAttribute("lesson", lesson);
+				// 編集画面を表示
 				return "lesson_edit.html";
 			}
 		}
@@ -58,24 +60,30 @@ public class LessonEditController {
 
 	// 更新処理をする
 	@PostMapping("/lesson/edit/process")
-	public String lessonUpdate(@RequestParam String imageName, @RequestParam String lessonName,
-			@RequestParam int price, @RequestParam LocalDate startDate, @RequestParam LocalTime finishTime,
-			@RequestParam LocalTime startTime, @RequestParam String description, @RequestParam Long lessonId,Model model) {
+	// @RequestParamはブラウザからのリクエストの値（パラメータ）を取得すること
+	public String lessonUpdate(@RequestParam String imageName, @RequestParam String lessonName, @RequestParam int price,
+			@RequestParam LocalDate startDate, @RequestParam LocalTime finishTime, @RequestParam LocalTime startTime,
+			@RequestParam String description, @RequestParam Long lessonId, Model model) {
 		// セッションからログインしている人の情報をadminという変数に格納
 		Admin admin = (Admin) session.getAttribute("loginAdminInfo");
+
 		// もし、admin == nullだったら、ログイン画面にリダイレクトする
-		// そうでない場合
-		// ファイルの保存
-		// もし、lessonUpdateの結果がtrueの場合は、講座変更完了にリダイレクト
-		// そうでない場合、講座編集画面にリダイレクトする
 		if (admin == null) {
+			// adminnameを表示
 			model.addAttribute("adminName", admin.getAdminName());
 			return "redirect:/admin/login";
+			// そうでない場合
 		} else {
-			if (lessonService.lessonUpdate(lessonId, startDate, startTime, finishTime, lessonName,description,  price,
+			// もし、lessonUpdateの結果がtrueの場合は
+			if (lessonService.lessonUpdate(lessonId, startDate, startTime, finishTime, lessonName, description, price,
 					imageName, admin.getAdminId())) {
+				// adminnameを表示
+				model.addAttribute("adminName", admin.getAdminName());
+				// 講座変更完了画面に遷移する
 				return "lesson_edit_complete.html";
+				// そうでない場合
 			} else {
+				// 講座編集画面にリダイレクトする
 				return "redirect:/lesson/edit";
 			}
 		}
